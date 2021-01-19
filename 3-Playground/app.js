3b8ec3bcff9a543dcb3e23fb532a7918387df7be
3DGISKing <wugis1219@gmail.com>
2021-01-19 20:55:52 +0800

    add playground

---
+++ 3-Playground/app.js
@@ -0,0 +1,99 @@
+const ArcType = Cesium.ArcType;
+const CallbackProperty = Cesium.CallbackProperty;
+const Cartesian2 = Cesium.Cartesian2;
+const Cartesian3 = Cesium.Cartesian3;
+const Cartographic = Cesium.Cartographic;
+const Cesium3DTileFeature = Cesium.Cesium3DTileFeature;
+const Cesium3DTileset = Cesium.Cesium3DTileset;
+const CesiumMath = Cesium.Math;
+const Color = Cesium.Color;
+const defined = Cesium.defined;
+const Ellipsoid = Cesium.Ellipsoid;
+const knockout = Cesium.knockout;
+const Matrix4 = Cesium.Matrix4;
+const Model = Cesium.Model;
+const PolylineArrowMaterialProperty = Cesium.PolylineArrowMaterialProperty;
+const Ray = Cesium.Ray;
+const HorizontalOrigin = Cesium.HorizontalOrigin;
+const Transforms = Cesium.Transforms;
+const VerticalOrigin = Cesium.VerticalOrigin;
+
+const defaultLongitude = 129.50778;
+const defaultLatitude = 42.9075;
+const defaultHeight = 100000;
+const defaultCartesian = Cartesian3.fromDegrees(defaultLongitude, defaultLatitude, defaultHeight);
+
+let viewModel = null;
+let viewer = null;
+
+function createModel() {
+    viewModel = {
+        cartesianX: defaultCartesian.x,
+        cartesianY: defaultCartesian.y,
+        cartesianZ: defaultCartesian.z,
+        longitude: defaultLongitude,
+        latitude: defaultLatitude,
+        height: defaultHeight
+    };
+
+    knockout.track(viewModel);
+
+    const toolbar = document.getElementById("toolbar");
+
+    knockout.applyBindings(viewModel, toolbar);
+
+    Cesium.knockout.getObservable(viewModel, 'cartesianX').subscribe(updateFromCartesian);
+    Cesium.knockout.getObservable(viewModel, 'cartesianY').subscribe(updateFromCartesian);
+    Cesium.knockout.getObservable(viewModel, 'cartesianZ').subscribe(updateFromCartesian);
+
+    Cesium.knockout.getObservable(viewModel, 'longitude').subscribe(updateFromDegrees);
+    Cesium.knockout.getObservable(viewModel, 'latitude').subscribe(updateFromDegrees);
+    Cesium.knockout.getObservable(viewModel, 'height').subscribe(updateFromDegrees);
+}
+
+function createViewer() {
+    viewer = new Cesium.Viewer("cesiumContainer", {
+        animation: false,
+        timeline: false
+    });
+
+    viewer.scene.globe.depthTestAgainstTerrain = true;
+}
+
+function updateFromDegrees() {
+    const longitude = viewModel.longitude;
+    const latitude = viewModel.latitude;
+    const height = viewModel.height;
+
+    const cartesian = Cartesian3.fromDegrees(longitude, latitude, height);
+
+    viewModel.cartesianX = cartesian.x;
+    viewModel.cartesianY = cartesian.y;
+    viewModel.cartesianZ = cartesian.z;
+}
+
+function updateFromCartesian() {
+    const x = parseFloat(viewModel.cartesianX);
+    const y = parseFloat(viewModel.cartesianY);
+    const z = parseFloat(viewModel.cartesianZ);
+
+    const carto = Cartographic.fromCartesian(Cesium.Cartesian3(x , y, z));
+
+    viewModel.longitude = CesiumMath.toDegrees(carto.longitude);
+    viewModel.latitude = CesiumMath.toDegrees(carto.latitude);
+    viewModel.height = carto.height;
+}
+
+function goTo() {
+    viewer.camera.flyTo({
+        destination : Cartesian3.fromDegrees(viewModel.longitude, viewModel.latitude, viewModel.height)
+    });
+}
+
+function main() {
+    createModel();
+    createViewer();
+    goTo();
+}
+
+main();
