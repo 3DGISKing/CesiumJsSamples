b366d97e11f63d31fa7a411b774a50b28f33bdb8
3DGISKing <wugis1219@gmail.com>
2021-03-22 16:35:11 +0800

    update UndergroundDrillHole sample

--- UndergroundDrillHole/app.js
+++ UndergroundDrillHole/app.js
@@ -1,3 +1,5 @@
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYTIzNzQ4NS1iN2ZmLTQ3ZWQtYjU0OS1mZWI4Nzk0MjcwNDAiLCJpZCI6OTc4Niwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1NDkxODE3NH0.UUQf2vuc3PN3VPNSUYt5uAbrSv5irvkIe-A57Ocp6ow";
+
 const Viewer = Cesium.Viewer;
 const createWorldTerrain= "cesium";
 const Cartesian3 = Cesium.Cartesian3;
