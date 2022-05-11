2a31af2dc1184f855a727ea9e2aa69e06d859596
3DGISKing <wugis1219@gmail.com>
2022-05-11 18:58:08 +0800

    add EntityPrimitiveAPI/Polygon/changeCoordinate.html

---
+++ .prettierrc.js
@@ -0,0 +1,45 @@
+// module.exports = {
+//   // Print spaces between brackets in object literals.
+//   bracketSpacing: true,
+
+//   // Carriage Return + Line Feed characters (\r\n), common on Windows
+//   endOfLine: "crlf",
+
+//   // Specify the line length that the printer will wrap on.
+//   printWidth: 120,
+
+//   // Print semicolons at the ends of statements.
+//   semi: true,
+
+//   // Use single quotes instead of double quotes.
+//   singleQuote: false,
+
+//   //Specify the number of spaces per indentation-level.
+//   tabWidth: 4,
+
+//   // No trailing commas.
+//   trailingComma: "none",
+// };
+
+module.exports = {
+    // Print spaces between brackets in object literals.
+    bracketSpacing: true,
+
+    // Carriage Return + Line Feed characters (\r\n), common on Windows
+    endOfLine: "crlf",
+
+    // Specify the line length that the printer will wrap on.
+    printWidth: 120,
+
+    // Print semicolons at the ends of statements.
+    semi: true,
+
+    // Use single quotes instead of double quotes.
+    singleQuote: false,
+
+    //Specify the number of spaces per indentation-level.
+    tabWidth: 4,
+
+    // No trailing commas.
+    trailingComma: "none"
+};
